26520d6ab49d1b7a8a4f93c9e95c409450b88e0b
Jeff Glass <glass.jeffrey@gmail.com>
2026-08-27 14:10:43 -0500

    Add cacheing for icons

--- src/flightmap/plane_icons.py
+++ src/flightmap/plane_icons.py
@@ -1,23 +1,48 @@
 # Maps plane categories from API to icons
 from PIL import Image, ImageFile
 from pathlib import Path
+from typing import Iterable
 
 from opensky_api import StateVector
 
+class ImageSpinner:
+    def __init__(self, img_path: Path | str, size=25):
+        self.img_path = Path(img_path)
+        self._base_img = Image.open(self.img_path)
+        self.size = size
+
+
+    def rotated(self, deg: int, obey_cache:bool = True) -> ImageFile.ImageFile:
+        cache_path = self.img_path.parent / self.img_path.stem / f"{self.img_path.stem}_{deg}.png"
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+
+        if obey_cache and cache_path.exists():
+            return Image.open(cache_path)
+
+        resized_and_rotated = _rotate_and_thumb(self._base_img, deg, self.size)
+        resized_and_rotated.save(cache_path)
+        return resized_and_rotated
+
+    def precache_rotations(self, deg_list: Iterable[int]) -> None:
+        for deg in deg_list:
+            _ = self.rotated(deg, obey_cache=True)
+        
+    
+
 plane_icons_base = {
-    2: Image.open(Path(r"icons\png\cessna.png")), # Light (< 15500 lbs),
-    3: Image.open(Path(r"icons\png\glf5.png")), # Small (15500 to 75000 lbs),
-    4: Image.open(Path(r"icons\png\a330.png")), # Large (75000 to 300000 lbs),
-    5: Image.open(Path(r"icons\png\b767.png")), # High Vortex Large (aircraft such as B-757),
-    6: Image.open(Path(r"icons\png\b787.png")), # Heavy (> 300000 lbs),
-    7: Image.open(Path(r"icons\png\a6.png")), # High Performance (> 5g acceleration and 400 kts),
-    8: Image.open(Path(r"icons\png\a7.png")), # Rotorcraft,
-    9: Image.open(Path(r"icons\png\b1.png")), # Glider / sailplane,
-    10: Image.open(Path(r"icons\png\f11.png")), # Lighter-than-air,
+    2: ImageSpinner(Path(r"icons\png\cessna.png")), # Light (< 15500 lbs),
+    3: ImageSpinner(Path(r"icons\png\glf5.png")), # Small (15500 to 75000 lbs),
+    4: ImageSpinner(Path(r"icons\png\a330.png")), # Large (75000 to 300000 lbs),
+    5: ImageSpinner(Path(r"icons\png\b767.png")), # High Vortex Large (aircraft such as B-757),
+    6: ImageSpinner(Path(r"icons\png\b787.png")), # Heavy (> 300000 lbs),
+    7: ImageSpinner(Path(r"icons\png\a6.png")), # High Performance (> 5g acceleration and 400 kts),
+    8: ImageSpinner(Path(r"icons\png\a7.png")), # Rotorcraft,
+    9: ImageSpinner(Path(r"icons\png\b1.png")), # Glider / sailplane,
+    10: ImageSpinner(Path(r"icons\png\f11.png")), # Lighter-than-air,
     # 11: ..., # Parachutist / Skydiver,
-    12: Image.open(Path(r"icons\png\b4.png")), # Ultralight / hang-glider / paraglider,
+    12: ImageSpinner(Path(r"icons\png\b4.png")), # Ultralight / hang-glider / paraglider,
     # 13: ..., # Reserved,
-    14: Image.open(Path(r"icons\png\b0.png")), # Unmanned Aerial Vehicle,
+    14: ImageSpinner(Path(r"icons\png\b0.png")), # Unmanned Aerial Vehicle,
     # 15: ..., # Space / Trans-atmospheric vehicle,
     # 16: ..., # Surface Vehicle – Emergency Vehicle,
     # 17: ..., # Surface Vehicle – Service Vehicle,
@@ -27,32 +52,37 @@ plane_icons_base = {
 }
 
 filler_plane_icons_base = (
-    Image.open(Path(r"icons\png\a0.png")),
-    Image.open(Path(r"icons\png\b737.png")),
-    Image.open(Path(r"icons\png\b747.png")),
-    Image.open(Path(r"icons\png\b777.png")),
-    Image.open(Path(r"icons\png\md11.png")),
-    Image.open(Path(r"icons\png\a330.png")),
-    Image.open(Path(r"icons\png\a340.png")),
-    Image.open(Path(r"icons\png\a380.png")),
+    ImageSpinner(Path(r"icons\png\a0.png")),
+    ImageSpinner(Path(r"icons\png\b737.png")),
+    ImageSpinner(Path(r"icons\png\b747.png")),
+    ImageSpinner(Path(r"icons\png\b777.png")),
+    ImageSpinner(Path(r"icons\png\md11.png")),
+    ImageSpinner(Path(r"icons\png\a330.png")),
+    ImageSpinner(Path(r"icons\png\a340.png")),
+    ImageSpinner(Path(r"icons\png\a380.png")),
 )
 
+def retieve_rotation(base_img: ImageFile.ImageFile, rot_list: list[int]) -> ImageFile.ImageFile:
+    ...
+
+def _rotate_and_thumb(img: ImageFile.ImageFile, r, size):
+    img = img.rotate(r)
+    img.thumbnail((size, size))
+    return img
+
 def make_rotations(base_img: ImageFile.ImageFile) -> dict[int, ImageFile.ImageFile]:
-    def _rotate_and_thumb(img: ImageFile.ImageFile, r, size):
-        img = img.rotate(r)
-        img.thumbnail((size, size))
-        return img
+
     return {r: _rotate_and_thumb(base_img, r, 25) for r in range(0, 361, 10)}
 
     
 print("Pre-generating rotations of included plane icons")
-plane_icons_rotated = {
-    index: make_rotations(base_image) for index, base_image in plane_icons_base.items()
-}
+for spinner in plane_icons_base.values():
+    spinner.precache_rotations(range(0, 361, 10))
 print("Finished generating standard plane icon rotations")
 
 print("Pre-generating rotations of additional plane icons")
-filler_icons_rotated = [make_rotations(base_image) for base_image in filler_plane_icons_base]
+for spinner in filler_plane_icons_base:
+    spinner.precache_rotations(range(0, 361, 10))
 print("Finished generating additions plane rotations")
 
 
@@ -60,21 +90,21 @@ def get_plane_icon(plane: StateVector) -> ImageFile.ImageFile:
     rot = 360 - (round(plane.true_track / 10) * 10)
 
     if plane.category is not None:
-        if plane.category in plane_icons_rotated:
-            img = plane_icons_rotated[plane.category][rot]
-            print(f"Using plane category {plane.category}")
+        if plane.category in plane_icons_base:
+            img = plane_icons_base[plane.category].rotated(rot)
+            #print(f"Using plane category {plane.category}")
         elif plane.category in (0, 1):
             if plane.icao24 is not None:
-                img = filler_icons_rotated[int(plane.icao24, 16) % len(filler_icons_rotated)][rot]
-                print(f"{plane.category=}, using 'random' filler icon from icao id")
+                img = filler_plane_icons_base[int(plane.icao24, 16) % len(filler_plane_icons_base)].rotated(rot)
+                #print(f"{plane.category=}, using 'random' filler icon from icao id")
             else:
-                img = filler_icons_rotated[0][rot]
-                print(f"{plane.category=}, but no icao id")
+                img = filler_plane_icons_base[0].rotated(rot)
+                #print(f"{plane.category=}, but no icao id")
         else:
-            img = filler_icons_rotated[0][rot]
-            print(f"No icon for {plane.category=}")
+            img = filler_plane_icons_base[0].rotated(rot)
+            #print(f"No icon for {plane.category=}")
     else:
-        img = filler_icons_rotated[0][rot]
-        print(f"{plane.category=}")
+        img = filler_plane_icons_base[0].rotated(rot)
+        #print(f"{plane.category=}")
 
     return img
